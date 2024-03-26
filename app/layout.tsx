f39c2db05c81a9e6fe19b9e07db779dcf9978ecd
eigofurniture <162261950+eigofurniture@users.noreply.github.com>
2024-03-26 16:45:55 +0700

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,50 @@
+import "./globals.css";
+
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import { SITENAME } from "@/lib/constants";
+import { ThemeProvider } from "@/components/theme-provider";
+
+const font = Nunito({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: `${SITENAME} - Video Bokep Indo Viral Terbaru`,
+    description: `${SITENAME} Video bokep indo jepang indonesia barat terbaru viral korea simontok china tante live sma paksa ngentot abg jilbab cewek bocil smp pijat pelajar.`,
+    metadataBase: new URL("https://bokepbarat.pages.dev"),
+    alternates: {
+        canonical: `/`,
+    },
+    openGraph: {
+        title: `${SITENAME} Video Bokep Indo Jepang Barat Viral Terbaru`,
+        description: `${SITENAME} Video bokep indo jepang indonesia barat terbaru viral korea simontok china tante live sma paksa ngentot abg jilbab cewek bocil smp pijat pelajar.`,
+        url: `/`,
+        type: `website`,
+    },
+    verification: {
+        google: 'a0iO9M1poim7iMPhh8X1uFamlHzws2BRrIXNv_kTVHI',
+        yandex: '0b51414c87634a06',
+    },
+};
+
+export const runtime = "edge";
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <body className={font.className}>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    {children}
+                </ThemeProvider>
+            </body>
+        </html>
+    );
+}
