cddc994fc353d191386fd20951535e7ffb1495f4
ruangsayur <162388318+ruangsayur@users.noreply.github.com>
2024-04-07 12:01:16 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-
+import Script from "next/script";
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import { SITENAME } from "@/lib/constants";
@@ -10,19 +10,19 @@ const font = Nunito({ subsets: ["latin"] });
 export const metadata: Metadata = {
     title: `${SITENAME} - Video Bokep Indo Viral Terbaru`,
     description: `${SITENAME} Video bokep indo jepang indonesia barat terbaru viral korea simontok china tante live sma paksa ngentot abg jilbab cewek bocil smp pijat pelajar.`,
-    metadataBase: new URL("https://bokepbarat.pages.dev"),
+    metadataBase: new URL("https://bokepidaman.pages.dev"),
     alternates: {
         canonical: `/`,
     },
     openGraph: {
-        title: `${SITENAME} Video Bokep Indo Jepang Barat Viral Terbaru`,
+        title: `${SITENAME} - Video Bokep Indo Jepang Barat Viral Terbaru`,
         description: `${SITENAME} Video bokep indo jepang indonesia barat terbaru viral korea simontok china tante live sma paksa ngentot abg jilbab cewek bocil smp pijat pelajar.`,
         url: `/`,
         type: `website`,
     },
     verification: {
-        google: 'a0iO9M1poim7iMPhh8X1uFamlHzws2BRrIXNv_kTVHI',
-        yandex: '0b51414c87634a06',
+        google: '_2M7aIipRYBOnrewppHKZarAfLj-t2WuEjJCdxLgg_0',
+        yandex: '9f9903212bba70bb',
     },
 };
 
@@ -33,6 +33,42 @@ export default function RootLayout({
 }: {
     children: React.ReactNode;
 }) {
+    const jsonLd = {
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "name": `${SITENAME}`,
+        "description": `${SITENAME} Video bokep indo jepang indonesia barat terbaru viral korea simontok china tante live sma paksa ngentot abg jilbab cewek bocil smp pijat pelajar.`,
+        "logo": "https://bokepidaman.pages.dev/favicon.ico",
+        "url": "https://bokepidaman.pages.dev",
+        "founder":{
+            "@type":"Person",
+            "name":"admin",
+            "url":"https://bokepidaman.pages.dev"
+            },
+            "foundingDate":"2024-01-26"
+        }
+        const jsonLd1 = {
+            "@context": "https://schema.org",
+        "@type": "WebPage",
+        "name": `${SITENAME}`,
+        "url": "https://bokepidaman.pages.dev",
+        "description": `${SITENAME} Video bokep indo jepang indonesia barat terbaru viral korea simontok china tante live sma paksa ngentot abg jilbab cewek bocil smp pijat pelajar.`,
+        "image": "https://bokepidaman.pages.dev/favicon.ico",
+        "potentialAction":{
+            "@type":"ReadAction",
+            "target":"https://bokepidaman.pages.dev/"}
+        }
+        const jsonLd2 = {
+            "@context": "https://schema.org",
+        "@type": "WebSite",
+        "name": `${SITENAME}`,
+        "url": "https://bokepidaman.pages.dev",
+        "description": `${SITENAME} Video bokep indo jepang indonesia barat terbaru viral korea simontok china tante live sma paksa ngentot abg jilbab cewek bocil smp pijat pelajar.`,
+        "potentialAction": { 
+        "@type": "SearchAction", 
+          "target": "https://bokepidaman.pages.dev/?q={search_term}", 
+            "query-input": "required name=search_term"}
+        }
     return (
         <html lang="en" suppressHydrationWarning>
             <body className={font.className}>
@@ -42,9 +78,22 @@ export default function RootLayout({
                     enableSystem
                     disableTransitionOnChange
                 >
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd1) }}
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd2) }}
+        />
                     {children}
                 </ThemeProvider>
             </body>
+<Script src="https://js.juicyads.com/jp.php?c=947403z2v256s2x2w264x294&u=http%3A%2F%2Fwww.juicyads.rocks"/>
         </html>
     );
 }
